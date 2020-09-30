cd62c2a8c14f262263e8b93fe17589b8bcca61f3
Leon Lee <64774630+leonlee603@users.noreply.github.com>
2020-09-30 13:47:58 +0800

    Update autofocus.js

--- js/autofocus.js
+++ js/autofocus.js
@@ -6,5 +6,5 @@ function editAutoFocus() {
     const el_album_title = document.getElementById("album_title");
     el_album_title.focus();
 }
-window.addEventListener("load", loginAutoFocus, false);
-window.addEventListener("load", editAutoFocus, false);
+window.addEventListener("DOMContentLoaded", loginAutoFocus, false);
+window.addEventListener("DOMContentLoaded", editAutoFocus, false);
